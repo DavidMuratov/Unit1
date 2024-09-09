f59d9ce43afff0caa5fdf5655739918443dd19fa
DavidMuratov <144723035+DavidMuratov@users.noreply.github.com>
2024-09-09 09:30:14 -0400

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,1 +1,19 @@
+# Hashtags are symbols to create comment
 
+fname = "David" #string variable
+age=16 # number variable
+fact = True # Boolean Variable
+
+print(10) #this prints a number
+print("We are the best") #prints string    
+    
+#print data from a variable
+
+print(fname)
+
+question = input("what day is it?")
+
+print(fname, "Happy " + question+ "!")
+
+
+ 
